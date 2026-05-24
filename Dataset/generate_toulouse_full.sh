1bbd944ddfdadc4436f896ed18bdc36c985345ce
Jiahao Xie <159833414+Jiahao-Xie-86@users.noreply.github.com>
2026-05-24 06:16:57 -0400

    Update generate_toulouse_full.sh

--- Dataset/generate_toulouse_full.sh
+++ Dataset/generate_toulouse_full.sh
@@ -10,7 +10,7 @@ echo ""
 cd /usa/jiahaox/Image2matrix_baselines
 
 python3 dataset/create_toulouse_benchmark_dataset.py \
-    --output_path dataset/processed/toulouse_benchmark_128x128 \
+    --output_path dataset/processed/toulouse_benchmark \
     --image_size 64 64
 
 echo ""
